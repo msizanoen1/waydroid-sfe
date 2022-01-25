2c786a1015fb5c7a28ee4efeaa07cf8a2277a978
msizanoen1 <msizanoen@qtmlabs.xyz>
2022-01-25 11:38:24 +0700

    Uses docker when podman is not available

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -ex
-podman build --layers -t waydroid-builder .
+if command -v podman >/dev/null 2>&1; then
+	runtime=podman
+	runtime_flags_build=(--layers)
+else
+	runtime=docker
+	runtime_flags_build=()
+fi
+"$runtime" build "${runtime_flags_build[@]}" -t waydroid-builder .
 rm -rf waydroid/dist dist
-podman run -v ./waydroid:/waydroid:z -w /waydroid waydroid-builder pyinstaller -s -y --add-data data:data ./waydroid.py "$@"
+"$runtime" run -v ./waydroid:/waydroid:z -w /waydroid waydroid-builder pyinstaller -s -y --add-data data:data ./waydroid.py "$@"
 if [ -d waydroid/dist/waydroid ]
 then
 	cp -rT waydroid/dist/waydroid dist
